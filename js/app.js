4b790a6cf8d27813ad5ff614a80d9e59bf60e717
JavaScriptNoob <Timur.bobylev@yandex.ru>
2018-12-09 20:42:41 +0100

    for git pages

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var myApp = angular.module('myApp',['firebase','ngRoute']);
+
+
+myApp.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl:"../src/home.html",
+            controller:'mainController'
+        })
+        .when("/login",{
+            templateUrl:"../src/login.html",
+            controller:"loginCtrl"
+        });
+});
+
+
+myApp.controller('mainController',function($scope){
+
+})
+myApp.controller('loginCtrl',function($scope){
+
+});
+
+ymaps.ready(init);
+
+function init() {
+    var geolocation = ymaps.geolocation,
+        myMap = new ymaps.Map('map', {
+            center: [55, 34],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+    // Сравним положение, вычисленное по ip пользователя и
+    // положение, вычисленное средствами браузера.
+    geolocation.get({
+        provider: 'yandex',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Красным цветом пометим положение, вычисленное через ip.
+        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+        result.geoObjects.get(0).properties.set({
+            balloonContentBody: 'Мое местоположение'
+        });
+        myMap.geoObjects.add(result.geoObjects);
+    });
+    var location = document.getElementById('get-location');
+    location.addEventListener('click', function(){  geolocation.get({
+        provider: 'browser',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Синим цветом пометим положение, полученное через браузер.
+        // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
+        result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
+        myMap.geoObjects.add(result.geoObjects);
+    });});
+   
+}
+
---
+++ js/controller.js
@@ -0,0 +1,40 @@
+
+angular.module('myApp').controller('authCtrl', 
+								  [	 
+								  	'$firebaseAuth', '$location',
+								  	iWillRuleOverTheWorld
+								  ]);
+
+function iWillRuleOverTheWorld ($firebaseAuth,$location) {
+	var someOccupiedCountries = this;
+	var auth = $firebaseAuth();
+
+	someOccupiedCountries.login = function(){
+	    auth.$signInWithPopup('google');
+	};
+
+	someOccupiedCountries.logout = function(){
+	    auth.$signOut();
+	};
+	
+	auth.$onAuthStateChanged(function(authData ){
+	   someOccupiedCountries.author = authData;
+	   //console.log(authData);
+	   if (authData) {
+		$location.url("/src/login.html");
+		$scope.$apply(); 
+	}
+	});
+
+	
+	}
+
+
+
+
+
+
+
+
+
+
